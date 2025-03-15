df127da469f2e68c4d20fc9b2ced20c018ea67b2
TonyThuyTu <quyngaisensua@gmail.com>
2025-03-15 10:55:26 +0700

    up

---
+++ lab2/src/app/hello-angular/hello-angular.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-angular',
+  standalone: false,
+  templateUrl: './hello-angular.component.html',
+  styleUrl: './hello-angular.component.css'
+})
+export class HelloAngularComponent {
+
+}
