4154c29a33dc55e6ae2666e4ee1e20ee558a1773
TonyThuyTu <quyngaisensua@gmail.com>
2025-03-19 14:10:54 +0700

    done lab2

--- lab2-angular/src/app/detail/detail.component.ts
+++ lab2-angular/src/app/detail/detail.component.ts
@@ -9,6 +9,6 @@ import { Component } from '@angular/core';
 export class DetailComponent {
   prices=["15.500.000đ"]
   olds = ["17.000.000đ"]
-  names = ["Iphone 15"]
+  names = ["Iphone 15 plus"]
   details = ["Thiết kế thời thượng và bền bỉ - Mặt lưng kính được pha màu xu hướng cùng khung viền nhôm bền bỉ"]
 }
